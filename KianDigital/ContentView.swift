f96d539d526223e4164318caad273088900a699b
Fatemeh <mailto:f.najafimoghadam@gmail.com>
2021-02-14 04:38:24 +0330

    - add network module

---
+++ KianDigital/ContentView.swift
@@ -0,0 +1,19 @@
+//
+// Created by Fatemeh Najafi on 2/14/21.
+// Copyright © 2021 risoapps. All rights reserved.
+ 
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KianDigital/KianDigitalApp.swift
@@ -0,0 +1,15 @@
+//
+// Created by Fatemeh Najafi on 2/14/21.
+// Copyright © 2021 risoapps. All rights reserved.
+
+
+import SwiftUI
+
+@main
+struct KianDigitalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
